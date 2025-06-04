2c127c02c370bfdf651762a96495d613621c76f7
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2025-06-04 20:08:25 +0300

    Part1: exercise1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const Statistics = ({stats}) => {
+  const { good, neutral, bad, total, average, positivefeedback } = stats;
+  return (
+    <>
+      <h3>Current Stastics</h3>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive Feedback: {positivefeedback}%</p>
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -12,19 +27,14 @@ const App = () => {
   const total = good + neutral + bad
   const average = total === 0 ? 0 : (good - bad) / total
   const positivefeedback = total === 0 ? 0 : good / total * 100
+  const stats = { good, neutral, bad, total, average, positivefeedback };
   return (
     <div>
       <h3>Give Feedback</h3>
         <button onClick={handleGoodClick}>Good</button>
         <button onClick={handleNeutralClick}>Neutral</button>
         <button onClick={handleBadClick}>Bad</button>
-      <h3>Current Stastics</h3>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total: {total}</p>
-        <p>Average: {average}</p>
-        <p>Positive Feedback: {positivefeedback}%</p>
+        <Statistics stats={stats}></Statistics>
     </div>
   )
 }
