93fca3473905597bcef1811466f5c8792e4bd49f
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2025-06-09 20:19:46 +0300

    Part2 : exercise 2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
+  const[newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addName = (event) => {
@@ -15,9 +20,10 @@ const App = () => {
     const similarNames = persons.filter((person) => newName === person.name)
     if (!similarNames.length)
     {
-      const nameObj = { name: newName }
+      const nameObj = { name: newName , number: newNumber}
       setPersons(persons.concat(nameObj))
       setNewName("")
+      setNewNumber("")
     }
     else
       alert(`${newName} is already added to phonebook`)
@@ -28,12 +34,15 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input type="text" placeholder='Enter name here' value={newName} onChange={handleNameChange}/>
+          name: <input type="text" placeholder='Enter name here' value={newName} onChange={handleNameChange} required/>
+        </div>
+        <div>
+          number: <input type="tel" placeholder='Enter number here' value={newNumber} onChange={handleNumberChange} required/>
         </div>
           <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => <p key={index}>{person.name}</p>)}
+      {persons.map((person, index) => <p key={index}>{person.name} {person.number}</p>)}
     </div>
   )
 }
