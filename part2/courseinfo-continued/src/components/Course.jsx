27f9e21b1773f0552154c8e13065404bc4bcc49a
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2025-06-09 11:15:42 +0300

    Part2: exercise2.2: Course information step 7

--- part2/courseinfo-continued/src/components/Course.jsx
+++ part2/courseinfo-continued/src/components/Course.jsx
@@ -1,11 +1,13 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
 const Course = (({course})=> {
     return (
         <>
             <Header course={course}></Header>
             <Content course={course}></Content>
+            <Total course={course}></Total>
         </>
     )
 })
