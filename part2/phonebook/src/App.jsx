2970c6e72d887f4b4d6ec5c72c0a76c2c07f693f
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2025-06-10 10:45:04 +0300

    Part2 : exercise 2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,8 +4,13 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-1234567'}
   ]) 
+
   const [newName, setNewName] = useState('')
   const[newNumber, setNewNumber] = useState('')
+  const[searchName, setSearchName] = useState('')
+  const[searchInput, setSearchInput] = useState('')
+  const [searchPerformed, setSearchPerformed] = useState(false);
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,12 +20,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchClick = (_) => {
+    setSearchName(searchInput);
+    setSearchInput('');
+    setSearchPerformed(true);
+  }
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
   const addName = (event) => {
     event.preventDefault()
+    setSearchPerformed(false)
+    setSearchName("")
     const similarNames = persons.filter((person) => newName === person.name)
     if (!similarNames.length)
     {
-      const nameObj = { name: newName , number: newNumber}
+      const nameObj = { name: newName , number: newNumber, id: persons.length}
       setPersons(persons.concat(nameObj))
       setNewName("")
       setNewNumber("")
@@ -29,9 +46,18 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
   }
 
+  const matchedPerson = persons.find((person) => person.name === searchName)
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input type="text" placeholder='Enter name here' value={searchInput} onChange={handleSearchInputChange} required/>
+        <button onClick={handleSearchClick}>Search</button>
+        {setSearchPerformed && matchedPerson && (<p>{matchedPerson.name} {matchedPerson.number}</p>)}
+        {setSearchPerformed && !matchedPerson && searchName && (<p>No matches found.</p>)}
+      </div>
+      <h2>Add phone numbers</h2>
       <form onSubmit={addName}>
         <div>
           name: <input type="text" placeholder='Enter name here' value={newName} onChange={handleNameChange} required/>
